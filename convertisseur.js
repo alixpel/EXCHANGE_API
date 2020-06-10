f598e05b46a08109fa535c0703b9f1f23f2e3819
Alix Pelletier <alixpelletierpro@gmail.com>
2020-06-10 19:08:23 +0200

    add exchange link

--- convertisseur.js
+++ convertisseur.js
@@ -1,3 +1,22 @@
+GET https:('//api.exchangeratesapi.io/latest HTTP/1.1')
+
+{
+  "base": "EUR",
+  "date": "2018-04-08",
+  "rates": {
+    "CAD": 1.565,
+    "CHF": 1.1798,
+    "GBP": 0.87295,
+    "SEK": 10.2983,
+    "EUR": 1.092,
+    "USD": 1.2234,
+    ...
+  }
+}
+
+// ========================================================================================
+
+
 function convertirEnEuros() {
  	let montantlivres = document.querySelector('.champlivres').value;
  	let montanteuros = (montantlivres * 1.15);
