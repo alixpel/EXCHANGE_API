26101ddcd023d262b860e31eff428e7a60b6ea9d
Alix Pelletier <alixpelletierpro@gmail.com>
2020-06-11 15:18:53 +0200

    fix typos

--- convertisseur.js
+++ convertisseur.js
@@ -1,25 +1,12 @@
-GET https:('//api.exchangeratesapi.io/latest HTTP/1.1')
+fetch('https://api.exchangeratesapi.io/latest HTTP/1.1')
 .then(function(body) {
   return body.json();
 })
-then(function(json) {
-  var span = document.getElemenById("GBP");
+.then(function(json) {
+  var span = document.getElementById("GBP");
   span.innerHTML = json.rates;
-})
+});
 
-{
-  "base": "EUR",
-  "date": "2018-04-08",
-  "rates": {
-    "CAD": 1.565,
-    "CHF": 1.1798,
-    "GBP": 0.87295,
-    "SEK": 10.2983,
-    "EUR": 1.092,
-    "USD": 1.2234,
-    ...
-  }
-}
 
 // ================ Convertisseur taux fictif : ================
 
